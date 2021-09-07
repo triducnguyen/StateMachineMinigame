82751ae42b683a5f7dcccc0fc3929a97f5f3f609
eldersquash <>
2021-09-07 09:39:30 -0600

    PS Tile Script Working

--- Photoshop Scripts/save_rule_tiles_test.js
+++ Photoshop Scripts/save_rule_tiles_test.js
@@ -14,6 +14,7 @@ var setLayers;
 var layerSets;
 
 var okBtn;
+var okEnabled = true;
 var cancelBtn;
 
 var layerDict = {};
@@ -97,7 +98,7 @@ function OnRuleDropDownChange()
 
 function UpdateOK()
 {
-    var enabled = !(baseLayerName.text == "" || ruleLayerName.text == "");
+    var enabled = !(baseLayerName.text == "" || ruleLayerName.text == "") && okEnabled;
     okBtn.enabled = enabled;
 }
 
@@ -199,12 +200,15 @@ function FilterLayers(layersIn, art)
 
 function ArtLayerNameCheck(name)
 {
+    
     //check if layer name matches Rule Tile naming
     return  Contains(name, 
                 [ 
-                    "TopLeft", "Top", "TopRight",
-                    "Left", "Right", "BottomLeft",
-                    "Bottom","BottomRight"
+                    "Top","Bottom",
+                    "Right", "Left",
+                    "Outer", "Inner",
+                    "Single","Double",
+                    "Triple", "Quad",
                 ]);
 }
 
@@ -219,64 +223,207 @@ function SetLayerNameCheck(name)
 
 function Contains(searchString, substrings)
 {
+    //alert("Checking "+searchString);
     for(var i = 0; i<substrings.length; i++)
     {
         var substring = substrings[i];
         if(searchString.indexOf(substring) >= 0)
         {
+            //alert(searchString+" contained "+substring);
             return true;
         }
     }
     return false;
-    //text += searchString+"-NoMatch";
 }
 
 function ExportRuleTiles(folder)
 {
-    //alert("exporting");
-    
-    //alert("New doc");
+
     var baseLayer = layerDict[baseLayerName.text];
-    var ruleLayer = layerDict[ruleLayerName.text];
-    
-    //var doc = app.activeDocument;
-
-    var pngOptions = new PNGSaveOptions();
-            pngOptions.compression = 0;
-            pngOptions.interlaced = false;
-
+    var rulesLayer = layerDict[ruleLayerName.text];
     //alert(ruleLayer.layers.length);
-    for(var i = 0; i<ruleLayer.layers.length; i++)
+    for(var i = 0; i<rulesLayer.layers.length; i++)
     {
-        tmpDoc = app.documents.add(width, height, 72, "tempDoc", NewDocumentMode.RGB);
-        var ilayer = ruleLayer.layers[i];
-        if(ilayer.typename == "ArtLayer")
+        
+        var ruleLayer = rulesLayer.layers[i];
+        if(ruleLayer.typename == "ArtLayer")
         {
-            alert(ilayer.name);
-            //create temporary container
-            var tempContainer = tmpDoc.layerSets.add();
-            //for some reason the layer you want to duplicate must be in the active document
-            app.activeDocument = baseDoc;
-            //duplicate base and rule template
-            var tempBase = baseLayer.duplicate(tempContainer, ElementPlacement.INSIDE);
-            var tempRule = ilayer.duplicate(tempBase,ElementPlacement.PLACEBEFORE);
-            //go back to tmp doc
-            app.activeDocument = tmpDoc;
-            //make sure layers are visible
-            tempBase.visible = true;
-            tempRule.visible = true;
-            //save document
-            
-            var imgName = ilayer.name;
-            var file = new File(decodeURI(folder.absoluteURI)+"/"+imgName+".png");
-            
-            tmpDoc.saveAs(file, pngOptions, true, Extension.LOWERCASE);
-            //alert("Saved "+ file.absoluteURI);
-            //cleanup
-            tmpDoc.close(SaveOptions.DONOTSAVECHANGES);
+            Export(folder, baseLayer, ruleLayer);
         }
     }
     alert("All done!");
+}
+
+function Export(folder, baseLayer, ruleLayer)
+{
+    //alert("exporting "+ruleLayer.name);
+    //Regular save tiles that don't need flipping
+    switch(ruleLayer.name)
+    {
+        case "Top":
+            Save(folder, baseLayer, ruleLayer, false);
+            break;
+        case "Quad":
+            Save(folder, baseLayer, ruleLayer, false);
+            break;
+        case "Bottom":
+            Save(folder, baseLayer, ruleLayer, false);
+            break;
+        case "TopDown":
+            Save(folder, baseLayer, ruleLayer, false);
+            break;
+        case "LeftRight":
+            Save(folder, baseLayer, ruleLayer, false);
+            break;
+        case "TrippleTop":
+            Save(folder, baseLayer, ruleLayer, false);
+            break;
+        case "TrippleBottom":
+            Save(folder, baseLayer, ruleLayer, false);
+            break;
+        case "QuadOuterCorner":
+            Save(folder, baseLayer, ruleLayer, false);
+            break;
+        case "OuterCornersTop":
+            Save(folder, baseLayer, ruleLayer, false);
+            break;
+        case "OuterCornersBottom":
+            Save(folder, baseLayer, ruleLayer, false);
+            break;
+        case "OuterCornersTop&Bottom":
+            Save(folder, baseLayer, ruleLayer, false);
+            break;
+        case "OuterCornersBottom&Top":
+            Save(folder, baseLayer, ruleLayer, false);
+            break;
+        default:
+            SaveBoth(folder, baseLayer, ruleLayer);
+            break;
+    }
+}
+
+function SaveBoth(folder, baseLayer, ruleLayer)
+{
+    //alert("saving original and flipped version");
+    Save(folder, baseLayer, ruleLayer, false);
+    Save(folder, baseLayer, ruleLayer, true);
+}
+
+function Save(folder, baseLayer, ruleLayer, flip)
+{
+    //alert("Saving");
+    var doc = CreateDoc();
+    //alert("before");
+    var layers = flip ?
+        DuplicateAndFlip(doc,baseLayer,ruleLayer) : Duplicate(doc, baseLayer, ruleLayer);
+    //alert("Before save");
+    SaveDocPNG(doc, folder, layers[1].name);
+}
+
+function Duplicate(doc, baseLayer, ruleLayer)
+{
+    //create temporary container
+    var tempContainer = doc.layerSets.add();
+    var lastActive = app.activeDocument;
+    //for some reason the layer you want to duplicate must be in the active document
+    app.activeDocument = baseDoc;
+    //duplicate base and rule template
+    var tempBase = baseLayer.duplicate(tempContainer, ElementPlacement.INSIDE);
+    var tempRule = ruleLayer.duplicate(tempBase, ElementPlacement.PLACEBEFORE);
+    //go back to previous document
+    app.activeDocument = doc;
+    //make sure layers are visible
+    tempBase.visible = true;
+    tempRule.visible = true;
+    //alert("regular dupe: "+tempRule.name)
+    return [tempBase, tempRule];
+}
+
+function DuplicateAndFlip(doc, baseLayer, ruleLayer)
+{
+    var bounds = [];
+    for(var i = 0; i<ruleLayer.bounds.length; i++)
+    {
+        var bound = ruleLayer.bounds[i];
+        bounds.push(bound.value);
+    }
+    //alert("duplicating and flipping")
+    var layers = Duplicate(doc, baseLayer, ruleLayer);
+    // Getting center coordinates of the document
+    var layerCenterOffsetX = bounds[2]/2;
+    var layerCenterOffsetY = bounds[3]/2;
+
+    var docCenterW = Math.round(width / 2);
+    var docCenterH = Math.round(height / 2);
+
+    var layerCenterX = Math.round(bounds[0]+layerCenterOffsetX);
+    var layerCenterY = Math.round(bounds[1]+layerCenterOffsetY);
+
+    var oDX = Math.round(docCenterW - layerCenterX);
+    var oDY = Math.round(docCenterH - layerCenterY);
+
+    var deltaX = 2*oDX;
+    var deltaY = bounds[1];
+    if(oDX != 0 || oDY != 0)
+    {
+        //I have no clue why, but subtracting the layerCenterOffsetX from oDX
+        //does not return the correct position. My code/math is wrong but I don't
+        //know where. I'm just going to leave it out and hope nothing breaks.
+        //var DX = deltaX-layerCenterOffsetX;   <-- doesnt work!!! stinky!!!
+        alert(
+            "DeltaX: "+deltaX+"\nLayerCenterOffsetX :"+ layerCenterOffsetX+
+            "\noriginal pos: "+bounds[0]+","+bounds[1]+
+            "\nnew pos: "+(bounds[0]+deltaX)+","+deltaY+
+            "\nWidth: "+bounds[2]+"\nHeight: "+bounds[3]);
+        
+        var xUV = new UnitValue(deltaX, "px");
+        var yUV = new UnitValue(deltaY, "px");
+        //alert(xUV.as("px")+"\n"+yUV.as("px"));
+        layers[1].translate(xUV, undefined);
+    }
+
+    
+
+    //alert("flip");
+    //flip
+    layers[1].resize(-100, undefined, AnchorPosition.MIDDLECENTER);
+
+    
+    //alert(layers[1].name);
+    layers[1].name = SwapName(layers[1].name);
+    //
+    return layers;
+}
+
+function SwapName(layerName)
+{
+    return layerName.replace(
+        /Left|Right/g,
+        function(match){return match == "Left" ? "Right" : "Left"}
+    );
+}
+
+function CreateDoc()
+{
+    return app.documents.add(width, height, 72, "tempDoc", NewDocumentMode.RGB);
+}
+
+function SaveDocPNG(doc, folder, name)
+{
+    //alert("saving");
+    var pngOptions = new PNGSaveOptions();
+            pngOptions.compression = 0;
+            pngOptions.interlaced = false;
+    
+    //save document
+    
+    var imgName = name;
+    var file = new File(decodeURI(folder.absoluteURI)+"/"+imgName+".png");
+    
+    doc.saveAs(file, pngOptions, true, Extension.LOWERCASE);
+    //alert("Saved "+ file.absoluteURI);
+    //cleanup
+    doc.close(SaveOptions.DONOTSAVECHANGES);
 }
 
 function SaveClicked()
