e4b5c98080031f97f7b1918989218804a28cd5a4
Brandon-bass <brandonbass@u.boisestate.edu>
2021-09-26 17:59:31 -0600

    b

--- StateMachineGame/Assets/Scripts/AI/AI/BirdAi.cs
+++ StateMachineGame/Assets/Scripts/AI/AI/BirdAi.cs
@@ -5,23 +5,27 @@ using System;
 
 public class BirdAi : AI
 {
+    public bool ReachedDest = false;
+    public bool ReachedDest1 = false;
+
     protected override void Awake()
     {
-        
         base.Awake();
-        //var fly = new FlyBehaviour();
-        var move = new WanderBehaviour(this, 3, 4f);
+        var fly = new FlyBehaviour(this);
+        var fly2 = new Fly2Behaviour(this);
+        //var move = new WanderBehaviour(this, 3, 4f);
         //var hungry = new HungryBehaviour(this, 1f);
 
-       // fly.enterConditions.Add(new Condition(() => energy <= 25));
-        move.enterConditions.Add(new Condition(() => energy <= 25));
-        move.exitConditions.Add(new Condition(() => energy > 25, move));
+        fly.enterConditions.Add(new Condition(() => energy >= 24)) ;
+       // fly2.enterConditions.Add(new Condition(()=> energy >= 24 && CheckForFood() != null));
+       /// fly.exitConditions.Add(new Condition(() => CheckForFood() != null));
 
         //hungry.exitConditions.Add(new Condition(() => energy <= 25));
 
-        behaviours.Add(move);
+       // behaviours.Add(move);
        // behaviours.Add(hungry);
-       // behaviour.Add(fly);
+       behaviours.Add(fly);
+        behaviours.Add(fly2);
     }
 
     protected override void Update()
